6cb84828be3be6df733585a58c331f1e708df718
ThePortDev <111320669+ThePortDev@users.noreply.github.com>
2023-01-17 14:52:27 -0700

    Base UI
    Nothing complicated, base folders and simple placeholders

---
+++ tamagacha/View/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  HomeView.swift
+//  tamagacha
+//
+//  Created by Porter Dover on 1/17/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("This is your house.")
+            Text("Pet placeholder")
+            Text("Menu placeholder")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
--- tamagacha/tamagachaApp.swift
+++ tamagacha/tamagachaApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct tamagachaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                MenuView()
+            }
         }
     }
 }

a0091f1c57ea40c156888055e306a3c95e882514
ThePortDev <111320669+ThePortDev@users.noreply.github.com>
2023-01-17 14:15:43 -0700

    Initial Commit

---
+++ tamagacha/tamagachaApp.swift
@@ -0,0 +1,17 @@
+//
+//  tamagachaApp.swift
+//  tamagacha
+//
+//  Created by Porter Dover on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct tamagachaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

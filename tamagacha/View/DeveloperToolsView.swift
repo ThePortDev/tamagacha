386b6fb0f6d592c966f2c3aceda705450f0a167f
PaytonGunnell <paytongunnell@gmail.com>
2023-01-19 13:40:08 -0700

    Put  Views and Extentions into their own files

--- tamagacha/View/DeveloperToolsView.swift
+++ tamagacha/View/DeveloperToolsView.swift
@@ -7,3 +7,15 @@
 
 import SwiftUI
 
+struct DeveloperToolsView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    var body: some View {
+        VStack {
+            Text("DevTools")
+            Button("Back") {
+                presentationMode.wrappedValue.dismiss()
+            }
+        }
+    }
+}
--- tamagacha/View/SettingsView.swift
+++ tamagacha/View/SettingsView.swift
@@ -7,3 +7,32 @@
 
 import SwiftUI
 
+struct SettingsView: View {
+    @State var enterCode = ""
+    @State var navigateToDevTools = false
+    var devCode = "cheesepuffs"
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    var body: some View {
+        VStack {
+            Text("Settings")
+            Button("Back") {
+                presentationMode.wrappedValue.dismiss()
+            }
+            
+            TextField("Code", text: $enterCode)
+                .multilineTextAlignment(.center)
+                .autocorrectionDisabled()
+                .onSubmit {
+                    if enterCode.lowercased() == devCode {
+                        navigateToDevTools = true
+                    }
+                }
+            
+        }
+        .navigate(to: DeveloperToolsView(), when: $navigateToDevTools)
+
+    }
+    
+}

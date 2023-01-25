5ee44e605695bda70eba22bcf7c04a4a580752a2
PaytonGunnell <paytongunnell@gmail.com>
2023-01-25 13:27:35 -0700

    StoreModel and StoreViewModel finished

--- tamagacha/View Model/StoreViewModel.swift
+++ tamagacha/View Model/StoreViewModel.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+class StoreViewModel: ObservableObject {
+    @Published var store: Store
+    
+    init() {
+        store = Store() //userDefaltStore.loadData()
+    }
+    
+    //MARK: Intents
+    func buy(item: String) {
+        store.buy(item: item)
+    }
+    func remove(item: String) {
+        store.remove(item: item)
+    }
+    func add(money: Int) {
+        store.add(money: money)
+    }
+}
